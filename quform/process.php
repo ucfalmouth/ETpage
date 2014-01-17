c68de6c3b9ed437a46a86b5494c992c352cda84e
Aaron Marr <aaronmarruk@gmail.com>
2014-01-17 10:29:34 +0000

    Debug form issues. Make form send

--- quform/process.php
+++ quform/process.php
@@ -39,7 +39,7 @@ $config['email'] = true;
  *     'recipient2@example.com'
  * );
  */
-$config['recipients'] = '';
+$config['recipients'] = 'aaron.marr@falmouth.ac.uk';
 
 /**
  * Set the "From" address of the emails. You should set this to the contact
@@ -59,7 +59,7 @@ $config['recipients'] = '';
  *
  * $config['from'] = '%email%';
  */
-$config['from'] = '';
+$config['from'] = '%email%';
 
 /**
  * The subject of the notification email message. %first_name% will be replaced
